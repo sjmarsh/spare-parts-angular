7850545d934d7babd6c36a7a3ff8ebc5114bd829
sjmarsh <sjmarsh31@gmail.com>
2023-11-28 15:49:12 +1100

    Add simple home component

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+  
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div>
+        <h1>Spare Parts - Angular</h1>
+        <p>This is a simple application to spike/demo various Angular features. </p>
+        <p>*Not intended to be a real-world inventory system :-)</p>
+    </div>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
