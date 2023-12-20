690086d7211703dfc32992de74080a4df1156cd2
sjmarsh <sjmarsh31@gmail.com>
2023-12-20 12:47:37 +1100

    Add SideNav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterLink, 
+    RouterLinkActive, 
+    RouterOutlet, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatListModule,
+    MatSidenavModule, 
+    MatToolbarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+// ref: https://dev.to/davidihl/how-to-create-a-responsive-sidebar-and-mini-navigation-with-material-angular-o5l#Import-required-components
+
 export class AppComponent {
   title = 'spare-parts-angular';
+  @ViewChild(MatSidenav)
+  sidenav!:MatSidenav;
+  isMobile = true;
+  isCollapsed = true;
+
+  constructor(private observer: BreakpointObserver){}
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if(screenSize.matches) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    })
+  }
+
+  toggleMenu() {
+    if(this.isMobile) {
+      this.sidenav.toggle();
+      this.isCollapsed = false;
+    } else {
+      this.sidenav.open();
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
 }
