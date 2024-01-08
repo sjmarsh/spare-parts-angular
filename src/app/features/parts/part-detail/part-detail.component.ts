e92e8b0fb5215a666ca085e6ccf9e60c2e3b6d58
sjmarsh <sjmarsh31@gmail.com>
2024-01-08 15:31:18 +1100

    More work on Part Detail modal

--- src/app/features/parts/part-detail/part-detail.component.ts
+++ src/app/features/parts/part-detail/part-detail.component.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 import Part from '../types/Part';
+import PartCategory from '../types/PartCategory';
 
 @Component({
   selector: 'app-part-detail',
@@ -13,22 +14,45 @@ import Part from '../types/Part';
   styleUrl: './part-detail.component.css',
   template: `
     
-      <form [formGroup]="partForm"] (ngSubmit)="onSubmit()">
+      <form [formGroup]="partForm" (ngSubmit)="onSubmit()">
         <h3>{{detailMode}}Part</h3>
         
-          <mat-form-field appearance="fill">
-            <mat-label>Enter Name<mat-label>
-            <input matInput placeholder="Name" formControlName="name" />
-            <mat-hint>Name<mat-hint>
-          </mat-form-field>
-
-          <mat-form-field appearance="fill">
-            <mat-label>Enter Description<mat-label>
-            <input matInput placeholder="Description" formControlName="description" />
-            <mat-hint>Description<mat-hint>
-          </mat-form-field>
-
+        <mat-form-field>
+          <mat-label>Enter Name</mat-label>
+          <input matInput formControlName="name">
+        </mat-form-field>
+          
+        <mat-form-field>
+          <mat-label>Description</mat-label>
+          <input matInput formControlName="description">
+        </mat-form-field>
         
+        <mat-form-field>
+          <mat-label>Category</mat-label>
+          <input matInput formControlName="category">
+        </mat-form-field>
+
+        <mat-form-field>
+          <mat-label>Weight</mat-label>
+          <input matInput type="number" formControlName="weight">
+        </mat-form-field>
+
+        <mat-form-field>
+          <mat-label>Price</mat-label>
+          <input matInput type="number" formControlName="price">
+          <span matTextPrefix>$&nbsp;</span>
+        </mat-form-field>
+
+        <mat-form-field>
+          <mat-label>Start Date</mat-label>
+          <input matInput type="date" formControlName="startDate">
+        </mat-form-field>
+
+        <mat-form-field>
+          <mat-label>End Date</mat-label>
+          <input matInput type="date" formControlName="endDate">
+        </mat-form-field>
+
       </form>
     
   `
@@ -37,10 +61,15 @@ export class PartDetailComponent {
   detailMode: string = 'Edit'
   partForm = new FormGroup({
     name: new FormControl(''),
-    description: new FormControl('')
+    description: new FormControl(''),
+    category: new FormControl(PartCategory.Electronic),
+    weight: new FormControl(1.1),
+    price: new FormControl(1.11),
+    startDate: new FormControl('2020-01-01'),
+    endDate: new FormControl('2024-10-10')
   })
   
-  part: Part = { name: 'Part 1', description: 'The first one' , weight: 1.1 } as Part
+  part: Part = { name: 'Part 1', description: 'The first one', category: PartCategory.Electronic, weight: 1.1, price: 1.11, startDate: '2020-01-01', endDate: '2024-10-10' } as Part
 
   onSubmit = () => {
     // TODO
--- src/app/features/parts/part-list/part-list.component.ts
+++ src/app/features/parts/part-list/part-list.component.ts
@@ -1,20 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'
 
 import { PartTableComponent } from '../part-table/part-table.component';
+import { PartDetailComponent } from '../part-detail/part-detail.component';
 
 @Component({
   selector: 'app-part-list',
   standalone: true,
-  imports: [CommonModule, PartTableComponent],
+  imports: [CommonModule, MatDialogModule, PartTableComponent, PartDetailComponent],
   template: `
     <div>
       <h3>Part List</h3>
       <app-part-table></app-part-table>
+      <button mat-raised-button (click)="showDetail()">Show Detail Test</button>
+      
     </div>
   `,
   styleUrl: './part-list.component.css'
 })
 export class PartListComponent {
+  
+  constructor(public dialog: MatDialog) {}
+
+  
+  showDetail = () => {
+    const dialogRef = this.dialog.open(PartDetailComponent);
+
+    dialogRef.afterClosed().subscribe(res => {
+      console.log('part detail dialog closed');
+    })
+  }
 
 }
