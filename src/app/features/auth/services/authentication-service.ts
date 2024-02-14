fbb515becc62b500fa03dfd0a65d403992d64ccd
sjmarsh <sjmarsh31@gmail.com>
2024-02-14 13:47:39 +1100

    Authentication service login now working with api

--- src/app/features/auth/services/authentication-service.ts
+++ src/app/features/auth/services/authentication-service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable, map, of } from "rxjs";
+import { Observable, catchError, map, of } from "rxjs";
 import AuthenticationRequest from "../types/AuthenticationRequest";
 import AuthenticationResponse from "../types/AuthenticationResponse";
-import { response } from "express";
 
 
 @Injectable({
@@ -11,30 +10,18 @@ import { response } from "express";
   })
 export class AuthenticationService {
     //const baseUrl = `${config.SERVER_URL}/api/part`;
-    private baseUrl: string = 'https://localhost:7104/api';
+    private baseUrl: string = 'https://localhost:7104/api/user';
     private authenticateUrl: string = `${this.baseUrl}/authenticate`;
     private refreshUrl: string = `${this.baseUrl}/refresh`;
 
-    constructor(private httpClient: HttpClient){
+    constructor(private httpClient: HttpClient) {
     }
 
     login = (request: AuthenticationRequest) : Observable<AuthenticationResponse> => {
         console.log('login service: login')
-        this.httpClient.post(this.authenticateUrl, request).pipe(
-            map(response => {
-                console.log(response)
-                return true;
-            })
-        )
-        // todo call api
-        
-        return of({
-            userName: request.userName,
-            displayName: "Administrator",
-            isAuthenticated: true,
-            message: 'logged in',
-            accessToken: 'amazing token'
-        } as AuthenticationResponse)
-        
+        const httpHeaders = new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+        return this.httpClient.post<AuthenticationResponse>(this.authenticateUrl, request, {headers: httpHeaders});
     }
 }
