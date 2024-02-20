98ad2f3a9d50cb89310a67a1d49287811a08513f
sjmarsh <sjmarsh31@gmail.com>
2024-02-20 13:21:44 +1100

    Setup environment config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
@@ -7,6 +7,8 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { environment } from '../environments/environment';
+
 import { authReducer } from './features/auth/store/auth.reducers';
 import { AuthEffects } from './features/auth/store/auth.effects';
 import { partListReducer } from './features/parts/store/partsList.reducers';
@@ -14,8 +16,20 @@ import { PartsListEffects } from './features/parts/store/partsList.effects';
 import { partReducer } from './features/parts/store/parts.reducers';
 import { PartsEffects } from './features/parts/store/parts.effects';
 
+export interface AppConfig {
+  serverUrl?: string;
+}
+
+export let APP_CONFIG = new InjectionToken<AppConfig>('app.config');  
+
+export let APP_CONFIG_DI: AppConfig = { 
+  serverUrl: environment.serverUrl
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
+
+    { provide: APP_CONFIG, useValue: APP_CONFIG_DI },
     provideHttpClient(),
     provideRouter(routes), 
     provideClientHydration(), 
