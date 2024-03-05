60245fbac67cdeaf49993135b1f434f28a565781
sjmarsh <sjmarsh31@gmail.com>
2024-03-05 15:55:49 +1100

    Get report to display + add progress bar

--- src/app/features/parts/part-report/part-report.component.ts
+++ src/app/features/parts/part-report/part-report.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,61 +15,47 @@ import { SafePipe } from '../../../infrastructure/safe-pipe';
 @Component({
     selector: 'app-part-report',
     standalone: true,
-    imports: [CommonModule, MatTooltipModule, MatButtonModule, MatIconModule, SafePipe],
+    imports: [CommonModule, MatProgressBarModule, MatTooltipModule, MatButtonModule, MatIconModule, SafePipe],
     styleUrl: './part-report.component.css',
     template: `
         <div class="pdf-container">
-            <!-- { reportFetchStatus === FetchStatus.Loading && 
-            <Spinner animation="border" role="status" >
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
-            }
-            {
-            errorMessage &&
-            <Alert variant='danger'>{errorMessage}</Alert>
-            }
-            { reportFetchStatus === FetchStatus.Failed && 
-                <Alert variant='danger'>An error occurred fetching report.</Alert>
-            } -->
+            <ng-container *ngIf="isLoading">
+                <mat-progress-bar mode="indeterminate"></mat-progress-bar>
+            </ng-container>
 
-            <p *ngIf="hasError">An error occurred fetching report.</p>
-
+            <ng-container *ngIf="hasError">
+                <p class="custom-error">An error occurred fetching report.</p>
+            </ng-container>
+            
             <div class='tool-container'>
                 <button mat-fab extended color="primary" class="tool-button" aria-label="Back" (click)="handleNavBack()" matTooltip="Back to Part List">
                     <mat-icon>arrow_back</mat-icon>
-                    Report
+                    Back
                 </button> 
             </div>
-            <div *ngIf="reportDataUrl">
-                <iframe src="{{reportDataUrl | safe}}" class="pdf-view"></iframe>
-            </div>
+            <ng-container *ngIf="reportDataUrl">
+                <iframe [src]="reportDataUrl | safe" class="pdf-view"></iframe>
+            </ng-container>
         </div>
     `
 })
 
 export class PartReportComponent {
     reportFetchStatus?: FetchStatus
-    reportData?: SafeResourceUrl
     reportDataUrl?: String
+    isLoading?: Boolean = this.reportFetchStatus && this.reportFetchStatus === FetchStatus.Loading
     hasError?: Boolean = this.reportFetchStatus && this.reportFetchStatus === FetchStatus.Failed
 
-    constructor(private store: Store<{partsReport: PartReportState}>, private router: Router, private sanitizer: DomSanitizer){
-        console.log('report construct')
+    constructor(private store: Store<{partsReport: PartReportState}>, private router: Router){
     }
 
     ngOnInit(): void {
-        console.log('report init')
         this.store.select(state => state.partsReport).subscribe(s => {
-            //console.log(s);
-            
             this.reportFetchStatus = s.fetchStatus;
             if(s.reportData){     
-                           
-                console.log('hey')
                 const blb = new Blob([s.reportData], {type: 'application/pdf'});
                 const url = URL.createObjectURL(blb);
-                //this.reportData = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-                this.reportDataUrl = url;
+                this.reportDataUrl = url;                
             }
         });
     }
