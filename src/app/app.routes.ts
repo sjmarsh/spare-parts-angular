d9d39a4373e6bdd9e950c31f28ed74544d884f9b
sjmarsh <sjmarsh31@gmail.com>
2024-04-09 08:45:49 +1000

    Add boilerplate components for Part Search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,10 @@ import { PartListComponent } from './features/parts/part-list/part-list.componen
 import { PartReportComponent } from './features/parts/part-report/part-report.component';
 import { InventoryHomeComponent } from './features/inventory/inventory-home/inventory-home.component';
 import { InventoryReportComponent } from './features/inventory/inventory-report/inventory-report.component';
+import { PartSearchComponent } from './features/search/part-search/part-search.component';
 import { partListGuard } from './features/auth/guards/part-list.guard';
 import { inventoryGuard } from './features/auth/guards/inventory-guard';
+import { partSearchGuard } from './features/auth/guards/part-search.guard';
 
 export const routes: Routes = [
     { path: 'home', title: 'Home', component: HomeComponent },
@@ -18,5 +20,6 @@ export const routes: Routes = [
     { path: 'part-report', title: 'Part Report', canActivate: [partListGuard], component: PartReportComponent },
     { path: 'inventory', title: 'Inventory', canActivate: [inventoryGuard], component: InventoryHomeComponent },
     { path: 'inventory-report', title: 'Inventory Report', canActivate: [inventoryGuard], component: InventoryReportComponent },
+    { path: 'part-search', title: 'Part Search', canActivate: [partSearchGuard], component: PartSearchComponent},
     { path: '**', component: HomeComponent }
 ];
