3232c39a6b68dd2e300f6bc1e09fe56d6fe80014
sjmarsh <sjmarsh31@gmail.com>
2025-01-29 15:24:11 +1100

    Fix message-box.component tests

--- src/app/components/message-box/message-box.component.spec.ts
+++ src/app/components/message-box/message-box.component.spec.ts
@@ -1,20 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MessageBoxComponent } from './message-box.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DebugElement } from '@angular/core';
 
 describe('MessageBoxComponent', () => {
   let component: MessageBoxComponent;
   let fixture: ComponentFixture<MessageBoxComponent>;
+  let el: DebugElement;
+  let matDialogRef;
+  const matDialogSpy = jasmine.createSpyObj('MatDialogRef', ['onNoClick', 'closeDialog']);
+  const data = {
+    title: 'Message Title',
+    message: 'The Message',
+    summary: 'The message summary'
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MessageBoxComponent]
+      imports: [MessageBoxComponent], 
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: matDialogSpy
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: data
+        }
+      ]
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(MessageBoxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(MessageBoxComponent);
+      component = fixture.componentInstance;
+      el = fixture.debugElement;
+      matDialogRef = TestBed.get(MatDialogRef);
+      fixture.detectChanges();
+    });
   });
 
   it('should create', () => {
